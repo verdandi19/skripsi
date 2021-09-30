b722652716d2fff00968d1adde852b5d90d94cab
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2021-09-30 14:59:40 +0700

    finishing

--- resources/views/Perhitungan/index.blade.php
+++ resources/views/Perhitungan/index.blade.php
@@ -41,7 +41,7 @@
                 <div class="w-full px-3">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                            for="grid-last-name">
-                        Support
+                        Confidence
                     </label>
                     <input value="0" name="confidence" id="confidence"
                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -121,7 +121,8 @@
             function elBodyConfidence(key, value, transaction) {
                 const {title, support, count, confidence} = value;
                 let minConfidence = $('#confidence').val();
-                let keterangan = confidence >= minConfidence ? 'Lolos' : 'Tidak Lolos';
+                let minSupport = $('#support').val();
+                let keterangan = (confidence >= minConfidence && support >= minSupport) ? 'Lolos' : 'Tidak Lolos';
                 return '<tr>' +
                     '<td>' + title + '</td>' +
                     '<td class="text-right">' + support + '%</td>' +
